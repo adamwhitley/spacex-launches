bc9ca9797b1717d129121d15b5e61f7ecea3d08f
adamwhitley <adam@adamwhitley.com>
2019-02-11 20:47:31 -0500

    Fixed: * Alternating L/R timeline * Chronological order bug * Unique key issue TODO: * Embedding all YT vids kills the page so lazy loading is preferable. Still need to add onclick to load video. * Finish getting Comments from promise and display on Launch item

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ const launchesQuery = `{
     launch_site {
       site_name
     }
+    links {
+      article_link
+      video_link
+    }
     rocket {
       rocket_name
     }
@@ -20,7 +24,13 @@ const launchesQuery = `{
 }`;
 
 const client = new GraphQLClient('https://api.spacex.land/graphql/');
+const commentsAPIKey = 'da2-tadwcysgfbgzrjsfmuf7t4huui';  // TODO: Make this inaccessible to client
 
+/**
+ * Ingests GQL data to state
+ * @param query
+ * @returns {{loading: boolean}}
+ */
 function useGraphQL(query) {
   const [state, setState] = useState({ loading: true });
 
@@ -38,6 +48,11 @@ function useGraphQL(query) {
   return state;
 }
 
+/**
+ * Initializes Header element
+ * @returns {*}
+ * @constructor
+ */
 function Header() {
   return (
     <div className="page-head">
@@ -46,6 +61,11 @@ function Header() {
   );
 }
 
+/**
+ * Initializes Progress Bar
+ * @returns {*}
+ * @constructor
+ */
 function Loading() {
   return (
     <div className="progress">
@@ -63,11 +83,20 @@ function Loading() {
   );
 }
 
+/**
+ * Grabs launches from data model and
+ * groups them by year then returns an
+ * UL of launches sorted by date.
+ * @param launches
+ * @returns {*}
+ * @constructor
+ */
 function Launches({ launches }) {
   const launchesByDate = launches.reduce((list, launch) => {
     const date = launch.launch_date_utc.slice(0, 4);
     list[date] = list[date] || [];
     list[date].push(launch);
+    list[date].sort((a, b) => a.id - b.id);
     return list;
   }, {});
 
@@ -77,7 +106,7 @@ function Launches({ launches }) {
         <span key={launchDate}>
           <li className="timeline-month">{launchDate}</li>
           {launchesByDate[launchDate].map(launch => (
-            <Launch key={launch.flight_number} launch={launch} />
+            <Launch key={launch.id} launch={launch} />
           ))}
         </span>
       ))}
@@ -85,21 +114,104 @@ function Launches({ launches }) {
   );
 }
 
-function Launch({ launch }) {
+/**
+ * Builds image from Video ID
+ * @param vID
+ * @returns {*}
+ * @constructor
+ */
+function VImg({ vID }) {
+  const vImg = `https://i.ytimg.com/vi/${vID}/hqdefault.jpg`;
+  return(<div className="vidHolder"><img src={vImg} alt="Launch Video" /><b>&#9656;</b></div>);
+  // TODO: Switch image w/ iframe code on click
+}
+
+/**
+ * Fetch GQL Comment data
+ * @param ID - Flight ID
+ * @returns {{loading: boolean}}
+ */
+function fetchComments(ID) {
+  const commentsEndpoint = "https://pb3c6uzk5zhrzbcuhssogcpq74.appsync-api.us-east-1.amazonaws.com/graphql";
+
+  const GQLClient = new GraphQLClient(commentsEndpoint, {
+    headers: {
+      'x-api-key': commentsAPIKey,
+    },
+  });
+
+  const query = `query getFlight($flightId: Int!) {
+      launchCommentsByFlightNumber(flightNumber: $flightId) {
+        items {
+          id
+          author
+          body
+          date
+        }
+      }
+    }`;
+
+  const variables = {
+    flightId: ID
+  };
+
+  const [state, setState] = useState({ loading: true });
+
+  useEffect(() => {
+    GQLClient.request(query, variables).then(
+        data => {
+          setState({ data, loading: false });
+        },
+        err => {
+          console.error(err);
+        }
+    );
+  }, [query]);
+
+  return state;
+}
+
+/**
+ * Renders Comments component
+ * @param ID
+ * @returns {Promise<void>}
+ */
+function Comments({ID}) {
+  const data = fetchComments(ID);
+  console.log(data);
+
+  // TODO: Build out comment response
+
+  return(<div className="comments">COMMENTS</div>);
+}
+
+/**
+ * Creates Launch object
+ * @param id - Destructured id from Launch object
+ * @param launch
+ * @returns {*}
+ * @constructor
+ */
+function Launch({ launch: {id,...launch} }) {
+  const [state, setState] = useState({ showComments: false });
   const launchIcon = launch.launch_success ? (
     <i className="icon mdi mdi-rocket" />
   ) : (
     <i className="icon mdi mdi-bomb" />
   );
 
+  let ytVid = null;
+  if (launch.links.video_link) {
+    ytVid = launch.links.video_link.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#]*).*/)[2];
+  }
+
   return (
-    <li className="timeline-item timeline-item-detailed right">
+    <li className={id % 2 === 0 ? "timeline-item timeline-item-detailed right" : "timeline-item timeline-item-detailed left"}>
       <div className="timeline-content timeline-type file">
         <div className="timeline-icon">{launchIcon}</div>
-
         <div className="timeline-header">
           <span className="timeline-autor">
-            #{launch.id}: {launch.mission_name}
+            #{id}: {launch.mission_name}
           </span>{' '}
           <p className="timeline-activity">
             {launch.rocket.rocket_name} &mdash; {launch.launch_site.site_name}
@@ -109,11 +221,22 @@ function Launch({ launch }) {
         <div className="timeline-summary">
           <p>{launch.details}</p>
         </div>
+        <div className="mediaPanel">
+          {ytVid ? <VImg vID={ytVid} /> : null}
+          {launch.links.article_link ? <a href={launch.links.article_link} target="_blank" rel="noopener noreferrer">Read Article</a> : null} |
+          <button type="button" className="commentsBtn" onClick={() => setState({ showComments: true })}>View Comments</button>
+          {state.showComments ? <Comments ID={id} /> : null}
+        </div>
       </div>
     </li>
   );
 }
 
+/**
+ * Renders page content
+ * @returns {*}
+ * @constructor
+ */
 export default function App() {
   const { data, loading } = useGraphQL(launchesQuery);
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import App from './App';
 import { render, cleanup, waitForElement } from 'react-testing-library';
-// TODO: resolve act warning https://github.com/kentcdodds/react-testing-library/issues/281
-// import { act } from 'react-dom/test-utils';
-
 afterEach(cleanup);
 
 test('renders launches', async () => {
